98a697b06fa3c8adc5823ed37f0ee94209537c08
deepakkumarjha1323 <deepakkumarjha1323@gmail.com>
2023-06-14 14:38:13 +0530

    l-38 functions

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// JavaScript Function
+
+/* A JavaScript function is defined with the function keyword, followed by a name, followed by parentheses ().
+Function names can contain letters, digits, underscores, and dollar signs (same rules as variables).
+ */
+
+// Function Syntax
+
+/*
+function <functionName>(){ - Formal Argument
+    function body
+
+    every function return somthing
+
+}
+
+<functionName>() - Actual Argument
+*/
+
+// Function defination is a one time process
+function ourMyFriend(name){ // Formal Argument
+    // Function Body
+    console.log('Hello '+ name);
+}
+// Function invoke/call is many time process
+ourMyFriend('Ram'); // Actual argument
+ourMyFriend("Hanuman"); // Actual Argument
+ourMyFriend(`Krishna`); // Actual Argument
